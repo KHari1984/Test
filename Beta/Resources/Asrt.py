57babfd04664b147c315ec00fb61af991f4f44df
Krishna Hari <Krishnambikac@gmail.com>
2017-09-05 07:45:52 -0600

    Add files via upload

---
+++ Beta/Resources/Asrt.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import collections
+import operator
+import csv
+from collections import defaultdict
+
+sys.path.insert(0, os.getcwd())
+
+
+def __init__(self):
+        pass
---
+++ Beta/Resources/FileUpload.au3
@@ -0,0 +1,3 @@
+ControlFocus("Open","","Edit1")
+ControlSetText("Open","","Edit1","C:\\Users\\kchandrasekharan\\Pictures\\Test Screen\\Ts001.JPG")
+ControlClick("Open","","Button1")
